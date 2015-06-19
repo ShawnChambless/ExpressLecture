86c2b1772eae9ce0bfcbe4c9ac26934f82c704e2
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-19 11:18:13 -0600

    Routes

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 'use strict';
 
 module.exports = function() {
-    var express = require('express');
-    var app = express();
+    var express = require('express'),
+    app = express(),
+    messages = [];
 
-    app.use(function(req, res) {
+    app.use('/', function(req, res, next) {
+        console.log('accessed app');
+        next();
+    });
 
-    var messages = [];
+    app.use('/secret', function(req, res) {
+        res.end('You found me!')
+    });
 
-    if (req.url === '/') {
-        res.end('wut');
-        return;
-    }
-    if(req.url === '/api/message' && req.method === 'GET') {
-        res.end(JSON.stringify(messages)) //if there were messages...
-        return;
-    }
-    if(req.url === '/api/message' && req.method === 'POST') {
+    app.use('/api', function(req, res) {
+        res.end('found api')
+    });
+    // /api
+    app.get('/api.message', function(req, res, next) {
+        res.end('messages')
+    });
+    app.post('/api.message', function(req, res) {
         var msg = '';
-        req.on('data', function() {
-            msg += chunk.toString('utf-8')
-        })
+        req.on('data', function(chunk) {
+            msg += chunk.toString('utf-8');
+        });
         req.on('end', function() {
             messages.push(msg)
             res.end('{success: true}')
-        })
-        return;
-    }
-    res.end('Hello');
+        });
     });
 return app;
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var http = require('http'),
     server = http.createServer(),
-    port = process.argv[2] || 3001,
+    port = process.argv[2] || 8081,
     app = require('./app');
 
 server.on('request', app);
